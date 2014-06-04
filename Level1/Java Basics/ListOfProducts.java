0cc0f202c472fcf3da291c1519b4e99cdeda7d84
vmaleshevska <vmaleshevska@users.noreply.github.com>
2014-06-04 16:16:39 +0300

    update

--- Level1/Java Basics/ListOfProducts.java
+++ Level1/Java Basics/ListOfProducts.java
@@ -1,7 +1,7 @@
 /*Create a class Product to hold products, which have name (string) and price (decimal number). 
  * Read from a text file named "Input.txt" a list of products. Each product will be in format name + space + price. 
  * You should hold the products in objects of class Product. Sort the products by price and write them in format 
- * price + space + name in a file named "Output.txt". Ensure you close correctly all used resources. */
+ * price + space + name in a file  named "Output.txt". Ensure you close correctly all used resources. */
 
 import java.io.File;
 import java.io.FileNotFoundException;
